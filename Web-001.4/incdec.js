b25130f85a3cba12907c76ed47a255eb8e934ffd
DBorisov <dborisov@luxoft.com>
2023-03-17 09:58:19 +0100

    4th

---
+++ Web-001.4/incdec.js
@@ -0,0 +1,9 @@
+let x=5;
+document.write("x=" + x++);
+document.write("<br>");
+document.write("x=" + ++x);
+document.write("<br>");
+document.write("x=" + x--);
+document.write("<br>");
+document.write("x=" + --x);
+document.write("<br>");
